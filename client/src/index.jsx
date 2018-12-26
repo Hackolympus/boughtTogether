e0c3faa1adff07641345fd1e283499e59214c5ad
chair <myleshu@gmail.com>
2018-12-26 18:59:43 -0500

    able to render on page change

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,23 +8,62 @@ class App extends React.Component {
     super();
     this.state = {
       currentItems: [],
-      currentPage: 0
+      currentPage: 0,
+      currentMax: 5
+    }
+    this.prevPage = this.prevPage.bind(this);
+    this.nextPage = this.nextPage.bind(this);
+    this.loadData = this.loadData.bind(this);
+  }
+  nextPage() {
+    if (Math.floor(this.state.currentItems.length /
+      ((this.state.currentPage + 1) * this.state.currentMax)) === 0) {
+      this.setState({
+        currentPage: 0
+      });
+    } else {
+      let temp = this.state.currentPage + 1;
+      this.setState({
+        currentPage: temp
+      })
+    }
+  }
+  prevPage() {
+    if (this.state.currentPage === 0) {
+      let temp = Math.floor(this.state.currentItems.length / this.state.currentMax);
+      this.setState({
+        currentPage: temp
+      })
+    } else {
+      let temp = this.state.currentPage - 1;
+      this.setState({
+        currentPage: temp
+      })
     }
   }
 
-  componentDidMount() {
+  loadData() {
     axios.get("/listing/1").then((response) => {
       this.setState({
-        currentItems: response.data
+        currentItems: response.data,
+        currentMax: Math.floor((window.innerWidth - 150) / 175)
       });
     }).catch((err) => console.log(err))
   }
+  
+  componentDidMount() {
+    this.loadData()
+  }
+
   render() {
     return (
       <div>Customers who bought this item also bought
-      <div>
-          <RelatedItems list={this.state.currentItems} page ={this.state.currentPage}/>
-        </div>
+          <RelatedItems
+          nextPage={this.nextPage}
+          prevPage={this.prevPage}
+          list={this.state.currentItems.slice
+            (this.state.currentPage * this.state.currentMax, 
+              this.state.currentPage * this.state.currentMax + this.state.currentMax)} />
       </div>
     );
   }
--- client/src/relatedItems.jsx
+++ client/src/relatedItems.jsx
@@ -4,24 +4,43 @@ class relatedItems extends React.Component {
   constructor(props) {
     super(props)
   };
+  
   render() {
     return (
 
-      <div>
+      <div className = "relCarousel">
+          <a className = "btn" id="btnPrevPage" href='#'
+          onClick ={this.props.prevPage}></a>
         {this.props.list.map(item => {
           return <div className='relItems' key={item.id} >
-            <a className="btnPrevPage" href='#'><img src={item.imageUrl}></img></a>
-            <div className='relItemsTitle'>{item.title}</div>
-            <div className='relItemsAuthor'>{item.author}</div>
-            <div className='relItemsRating'>{item.rating}</div>
-            <div className='relItemsRatingCount'>{item.ratingCount}</div>
-            <div className='bookType'>{item.bookType}</div>
-            <div className='relItemsPrice'>${item.priceDollars}.{item.priceCents}</div>
-            <div className='relItemsPrime'>{item.prime}</div>
+            <img src={item.imageUrl} className ="thumb"></img>
+            <div className='relitem' id='relItemsTitle'>{item.title}</div>
+            <div className='relitem' id='relItemsAuthor'>{item.author}</div>
+            <div className='relitem' id='relItemsRating'>{item.rating}</div>
+            <div className='relitem' id='relItemsRatingCount'>{item.ratingCount}</div>
+            <div className='relitem' id='bookType'>{item.bookType}</div>
+            <div className='relitem' id='relItemsPrice'>${item.priceDollars}.{item.priceCents}</div>
+            <div className='relitem' id='relItemsPrime'>{item.prime}</div>
           </div>
         })}
+          <a className = "btn" id="btnNextPage" href='#'
+          onClick ={this.props.nextPage}></a>
       </div>
     )
   };
 }
 export default relatedItems;
+
+/* {this.props.list.map(item => {
+  return <div className='relItems' key={item.id} >
+    <a className="btnPrevPage" href='#'><img src={item.imageUrl}></img></a>
+    <div className='relItemsTitle'>{item.title}</div>
+    <div className='relItemsAuthor'>{item.author}</div>
+    <div className='relItemsRating'>{item.rating}</div>
+    <div className='relItemsRatingCount'>{item.ratingCount}</div>
+    <div className='bookType'>{item.bookType}</div>
+    <div className='relItemsPrice'>${item.priceDollars}.{item.priceCents}</div>
+    <div className='relItemsPrime'>{item.prime}</div>
+  </div>
+})}
+ */
